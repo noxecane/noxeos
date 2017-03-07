908bb9c0dcd3e0de848cd416556e4bb95326d903
Arewa Olakunle <danceasarxx@gmail.com>
2017-03-07 18:10:51 +0100

    Are we sure multiboot is supported at all
    Added support for functions using 64byte stack. One of the functions
    is the multiboot check. Also added generic error function for these
    Modernizer like functions.

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -1,9 +1,36 @@
 global start
 
+
+; our stack for function return addresses
+section .bss
+stack_bottom:
+  resb 64
+stack_top:
+
+
 section .text
 bits 32
+
+; Prints 'ERR: $(al)'.
+error:
+  mov dword [0xb8000], 0x4f524f45
+  mov dword [0xb8004], 0x4f3a4f52
+  mov dword [0xb8008], 0x4f204f20
+  mov byte  [0xb800a], al
+  hlt
+
 start:
   ; hi
+  mov esp, stack_top
   mov dword [0xb8000], 0x2f4b2f4f
   hlt
 
+; making sure multiboot is supported
+check_multiboot:
+  cmp eax, 0x36d76289 ; compare eax to magic multiboot number and set flags register
+  jne .no_multiboot   ; jump to multiboot error message if no flag is set
+  ret                 ; return otherwise
+
+.no_multiboot:
+  mov al, "0" ; write 0 to al
+  jmp error   ; call error code(no need to call since error has no "ret")
